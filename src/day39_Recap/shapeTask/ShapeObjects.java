643b40d9a0002d9bc0ce74a3f85be6f2a54cc878
Olesia Sokol <lesy0101@me.com>
2022-01-14 14:48:02 -0500

    Objects v2

--- src/day39_Recap/shapeTask/ShapeObjects.java
+++ src/day39_Recap/shapeTask/ShapeObjects.java
@@ -3,9 +3,18 @@ package day39_Recap.shapeTask;
 public class ShapeObjects {
     public static void main(String[] args) {
 
-        Square square1 = new Square(-10);
+        Square square1 = new Square(10);
+        System.out.println(square1);
+        square1.setSide(20);
         System.out.println(square1);
 
+        System.out.println("--------------");
+        Rectangle rectangle = new Rectangle(5,6);
+
+        System.out.println(rectangle);
+        rectangle.setLength(10);
+        System.out.println(rectangle);
+        System.out.println(rectangle.getLength());
 
 
     }
