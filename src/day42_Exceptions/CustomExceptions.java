a906fdc9106447a35063f6722842a53a01290c6e
Olesia Sokol <lesy0101@me.com>
2022-01-27 23:00:19 -0500

    day42_Exceptions

---
+++ src/day42_Exceptions/CustomExceptions.java
@@ -0,0 +1,12 @@
+package day42_Exceptions;
+
+class FadyException extends RuntimeException{
+    public FadyException(String message){
+        super(message);
+    }
+}
+public class CustomExceptions {
+    public static void main(String[] args) {
+        throw new FadyException("It's time for break");
+    }
+}
