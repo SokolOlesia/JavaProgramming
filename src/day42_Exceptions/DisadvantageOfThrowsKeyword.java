0475c55111019d534f6aa654020ec555882350e5
Olesia Sokol <lesy0101@me.com>
2022-01-27 21:40:51 -0500

    day42_Exceptions

---
+++ src/day42_Exceptions/DisadvantageOfThrowsKeyword.java
@@ -0,0 +1,18 @@
+package day42_Exceptions;
+
+public class DisadvantageOfThrowsKeyword {
+    public static void sleep(double seconds) throws InterruptedException {
+        Thread.sleep((long) (seconds * 1000));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello");
+        sleep(0.5); //throws
+        System.out.println("Hello world");
+        System.out.println("-----------------");
+        System.out.println("Hello");
+        MorningWorkOut.sleep(0.5);// try/catch
+        System.out.println("Hello world");
+    }
+}
+
