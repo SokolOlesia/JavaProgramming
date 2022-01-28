53e6886341bec270ce6ba50e992f26d4e17ef8ad
Olesia Sokol <lesy0101@me.com>
2022-01-27 23:08:00 -0500

    day42_Exceptions

--- src/day42_Exceptions/CustomExceptions.java
+++ src/day42_Exceptions/CustomExceptions.java
@@ -1,12 +1,28 @@
 package day42_Exceptions;
 
-class FadyException extends RuntimeException{
-    public FadyException(String message){
+//custom unchecked exceptions
+class FadyException extends RuntimeException {
+    public FadyException(String message) {
         super(message);
     }
+
+    public FadyException() {
+        super("Time for short break");
+    }
 }
+
+class NoBreakException extends Exception {
+
+}
+
 public class CustomExceptions {
     public static void main(String[] args) {
-        throw new FadyException("It's time for break");
+
+        //throw new FadyException("It's time for lunch");
+        try {
+            throw new NoBreakException(); // unhandled exception
+        } catch (NoBreakException e) {
+            e.printStackTrace();
+        }
     }
 }
