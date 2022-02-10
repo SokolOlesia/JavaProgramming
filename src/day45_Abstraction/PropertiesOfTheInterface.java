f57ca7354f58d0ae395afa22d225fd6e111554ac
Olesia Sokol <lesy0101@me.com>
2022-02-10 22:26:56 +0700

    day45_Abstraction

--- src/day45_Abstraction/PropertiesOfTheInterface.java
+++ src/day45_Abstraction/PropertiesOfTheInterface.java
@@ -1,4 +1,30 @@
 package day45_Abstraction;
 
-public class PropertiesOfTheInterface {
+public interface PropertiesOfTheInterface {
+
+    int a = 100; //static & final by default
+    static int b = 200; // final by default
+
+//    public PropertiesOfTheInterface(int a) {
+//        this.a = a;
+//    }
+//    static {
+//        b = 100;
+//    }
+//    public void method1(){
+//        System.out.println("Instance method");
+//    }
+    public static void method2(){
+        System.out.println("Static method");
+    }
+    public abstract void method3();
+
+    public default void method4(){
+        System.out.println("Default method");
+    }
+    void drink();
+
+    default void method(){
+        System.out.println("Default method");
+    }
 }
