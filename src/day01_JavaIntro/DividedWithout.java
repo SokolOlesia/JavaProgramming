35d5c2845f04aea36eeb7cb39ba1f7f17f8abd34
Olesia Sokol <lesy0101@me.com>
2022-02-16 22:46:47 +0700

    day45_Abstraction

---
+++ src/day01_JavaIntro/DividedWithout.java
@@ -0,0 +1,20 @@
+package day01_JavaIntro;
+
+public class DividedWithout {
+    public static void main(String[] args) {
+        divide(15,3);
+    }
+    public static void divide(int num1, int num2) {
+        if (num2 == 0) {
+            System.out.println("Invalid Number");
+            return;
+        }
+
+        int count = 0;
+        while (num1 >= num2) {
+            num1 -= num2;
+            count++;
+        }
+        System.out.println(count);
+    }
+}
