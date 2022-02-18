9d2fe589d4b4929ff964c0b6bd3194bdbc1d6208
Olesia Sokol <lesy0101@me.com>
2022-02-18 13:21:53 +0700

    day48_Collections

--- src/day01_JavaIntro/DividedWithout.java
+++ src/day01_JavaIntro/DividedWithout.java
@@ -2,9 +2,9 @@ package day01_JavaIntro;
 
 public class DividedWithout {
     public static void main(String[] args) {
-        divide(15,3);
+        divideWithout(15,0);
     }
-    public static void divide(int num1, int num2) {
+    public static void divideWithout(int num1, int num2) {
         if (num2 == 0) {
             System.out.println("Invalid Number");
             return;
