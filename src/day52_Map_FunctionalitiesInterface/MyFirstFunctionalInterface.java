22721c525672d8fcdbbe3dac3a1808c0506673e4
Olesia Sokol <lesy0101@me.com>
2022-03-07 10:36:29 -0500

    day53_FunctionalInterface

---
+++ src/day52_Map_FunctionalitiesInterface/MyFirstFunctionalInterface.java
@@ -0,0 +1,9 @@
+package day52_Map_FunctionalitiesInterface;
+
+@FunctionalInterface
+public interface MyFirstFunctionalInterface {
+
+    void apply(int n);
+
+
+}
---
+++ src/day52_Map_FunctionalitiesInterface/MySecondFunctionalInterface.java
@@ -0,0 +1,8 @@
+package day52_Map_FunctionalitiesInterface;
+
+@FunctionalInterface
+public interface MySecondFunctionalInterface<T> {
+
+    void test(T data);
+
+}
